98f9242d5da0e03d0778a82ed637df20969fc22a
Stanislav Apanovich <apanovich@segater.com>
2015-02-06 18:43:58 +0200

    using Open3, architeck one param - one query

--- vzlist.rb
+++ vzlist.rb
@@ -1,35 +1,49 @@
 require 'logger'
-
+require 'open3'
+require 'json'
 
 class Vzlist
-	$LOG=Logger.new('log/app.log', 'daily')
-	@json_res 
-	@command
-	def initialize (ssh = false, *ssh_credentials)
+	$LOG = Logger.new('log/app.log', 'daily')
+	@ssh_string
+	@remote
+	def initialize (ssh = false, *ssh_cred)
+		@options = options
+		@remote = ssh 
 		if not (ssh)
 		    $LOG.debug('Use local vzlist') 
-			@command = 'vzlist'
-		elsif ssh_credentials.count == 3
+		elsif ssh_cred.count == 3
 			$LOG.debug('Use remote vzlist via ssh')
-			@command = 'ssh '+ssh_credentials[0].to_s+'@'+ssh_credentials[1].to_s+' -p '+ssh_credentials[2].to_s+' "sudo vzlist -jo ctid"'
-			$LOG.debug("initial line for ssh connection: '#{@command}'")
+			@ssh_string = 'ssh '+ssh_cred[0].to_s+'@'+ssh_cred[1].to_s+' -p '+ssh_cred[2].to_s
+			$LOG.debug("initial line for ssh connection: '#{@ssh_string}'")
         else
         	$LOG.fatal("initialization fault")
+        	abort
         end
+        self.execute_command
 	end
 	
     def execute_command ()
-    	#begin
-    		@json_res = system(@command)
-    	#rescue Exception => error
-    		#$LOG.fatal(error)
-    	#end
-    	#puts @json_res
-    	#puts $?.exitstatus
+    	runtime = @ssh_string+' "sudo vzlist '+@options.to_s+' "' if @remote
+    	runtime = '"sudo vzlist '+@options.to_s+' "' if not(@remote)
 
-    end    
+    	$LOG.debug("excute: #{runtime}")
+    	
+    	Open3.popen3(runtime) do | stdin, stdout, stderr, wait_thr |
+    		if wait_thr.value == 0 
+    			result = stdout.read
+    			$LOG.debug("Json from vzlist took. All normal")
+    		else 
+    			erro_return = stderr.read
+    			$LOG.fatal("Take error. Error - #{erro_return}")
+    		end
+    	end
+    	return 
+    end
+ 
+
 end
 
 
-test = Vzlist.new(true,"apanovich","88.85.72.50","21223")
-test.execute_command
+test = Vzlist.new("-jo ctid",true,"apanovich","88.85.72.50","21222")
+#test = Vzlist.new()
+#test.parse_result
